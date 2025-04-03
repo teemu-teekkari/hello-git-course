c98acb4e219d95ec5e2d275a6438b8a78f4477c0
Hurja1 <anttiuusoksa@gmail.com>
2025-04-03 20:03:46 +0300

    New version of mergesort.py

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,4 +1,11 @@
-print("{}: {}".format(key, value))
+
+def debug_print(debug_msg=None, **kwargs):
+
+    if debug_msg:
+        print(debug_msg)
+
+    for key, value in kwargs.items():
+        print("{}: {}".format(key, value))
 
 
 def mergesort(array):
@@ -37,10 +44,14 @@ def merge(left, right):
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
-
+    input_list = input_str.split(",")
+    value_list = []
+    for x in input_list:
+        try:
+            value_list.append(int(x))
+        except ValueError as err:
+            print("Invalid input.")
             quit(1)
-
-    debug_print(value_list=value_list)
 
     sorted_list = mergesort(value_list)
     print(sorted_list)
